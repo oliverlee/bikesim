346cc6e64b78e627dedbf81f8813eb7f05af68b8
Tiago Pinto <pinttiago@gmail.com>
2014-11-20 14:28:21 +0100

    Created the project, the levelScene and tried to move a cube, a sphere and a wheel

---
+++ game/Assets/Scripts/MovingSphereScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingSphereScript : MonoBehaviour {
+
+	public WheelCollider wheelColider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		float r = Input.GetAxis ("Rotation");
+
+		if (h != 0) {
+			wheelColider.steerAngle = 10* h;		
+		}
+		
+		if (v != 0) {
+			wheelColider.motorTorque += 500*v;	
+		}
+	}
+}
