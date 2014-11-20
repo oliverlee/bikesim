e28a05de72bd1aef93f97d90dd1af8d1c024b33b
Tiago Pinto <pinttiago@gmail.com>
2014-11-20 14:50:42 +0100

    Added a wheel with the proper script, correct the sphere script

--- game/Assets/Scripts/MovingWheelScript.cs
+++ game/Assets/Scripts/MovingWheelScript.cs
@@ -1,30 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovingSphereScript : MonoBehaviour {
+public class MovingWheelScript : MonoBehaviour {
+
+	public WheelCollider wheelColider;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
+	
 	void FixedUpdate () {
-
+		
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
 		float r = Input.GetAxis ("Rotation");
-
+		
 		if (h != 0) {
-			this.rigidbody.AddTorque(0,h,0);	
+			wheelColider.steerAngle = 10* h;		
 		}
 		
 		if (v != 0) {
-			this.rigidbody.AddRelativeTorque(v,0,0);	
+			wheelColider.motorTorque += 500*v;	
 		}
 	}
 }
