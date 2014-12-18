28abc59d2c30b6e5c5533e007ced926323df4ed2
Panchamy <kpanchamy@gmail.com>
2014-12-18 18:19:06 +0100

    Create opponent and opponent AI
    The opponent appears when left mouse button is clicked and the position
    is relative to the main bike. For now, the opponent follows the path
    created by only 2 waypoints.
    
    Improvements to be done : update the waypoints position according to the
    bike.

---
+++ game/Assets/Opponent.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var opponent : GameObject;
+var waypoint1 : GameObject;
+var waypoint2 : GameObject;
+
+var pos : Vector3;
+var rot : Quaternion;
+
+function Start () {
+	pos = transform.position;
+	pos.z = pos.z + 2;
+	rot = Quaternion.identity;
+}
+
+function Update () {
+	if (Input.GetMouseButtonDown(1)){
+		Instantiate(opponent, pos, rot);
+		pos.y = 0;
+		pos.x = pos.x + 2;
+		var w1 = Instantiate(waypoint1, pos, rot);
+		var w2 = Instantiate(waypoint2, pos + new Vector3(5,0,0), rot);
+		w1.name = "Waypoint0";
+		w2.name = "Waypoint1";
+		}
+}
---
+++ game/Assets/OpponentAI.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using System.Collections;
+
+public class OpponentAI : MonoBehaviour
+{
+	
+	public Transform[] waypoints;
+	public float waypointRadius = 1.5f;
+	public float damping = 0.1f;
+	public bool loop = false;
+	public float speed = 2.0f;
+	public bool faceHeading = true;
+	private int n;
+	
+	private Vector3 currentHeading,targetHeading;
+	private int targetwaypoint;
+	private Transform xform;
+	private bool useRigidbody;
+	private Rigidbody rigidmember;
+	
+	
+	// Use this for initialization
+	protected void Start ()
+	{	
+		n = 2;
+		waypoints=new Transform[n]; 
+		for (int i=0; i<n; i++) { 
+			waypoints [i] = (GameObject.Find("Waypoint"+i)).transform;
+		}
+		xform = transform;
+		currentHeading = xform.forward;
+		if(waypoints.Length<=0)
+		{
+			Debug.Log("No waypoints on "+name);
+			enabled = false;
+		}
+		targetwaypoint = 0;
+		if(rigidbody!=null)
+		{
+			useRigidbody = true;
+			rigidmember = rigidbody;
+		}
+		else
+		{
+			useRigidbody = false;
+		}
+	}
+	
+	
+	// calculates a new heading
+	protected void FixedUpdate ()
+	{
+		targetHeading = waypoints[targetwaypoint].position - xform.position;
+		
+		currentHeading = Vector3.Lerp(currentHeading,targetHeading,damping*Time.deltaTime);
+	}
+	
+	// moves us along current heading
+	protected void Update()
+	{
+		if(useRigidbody)
+			rigidmember.velocity = currentHeading * speed;
+		else
+			xform.position +=currentHeading * Time.deltaTime * speed;
+		if(faceHeading)
+			xform.LookAt(xform.position+currentHeading);
+		
+		if(Vector3.Distance(xform.position,waypoints[targetwaypoint].position)<=waypointRadius)
+		{
+			targetwaypoint++;
+			if(targetwaypoint>=waypoints.Length)
+			{
+				targetwaypoint = 0;
+				if(!loop)
+					enabled = false;
+			}
+		}
+	}
+	
+	
+	// draws red line from waypoint to waypoint
+	public void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		if(waypoints==null)
+			return;
+		for(int i=0;i< waypoints.Length;i++)
+		{
+			Vector3 pos = waypoints[i].position;
+			if(i>0)
+			{
+				Vector3 prev = waypoints[i-1].position;
+				Gizmos.DrawLine(prev,pos);
+			}
+		}
+	}
+	
+}
