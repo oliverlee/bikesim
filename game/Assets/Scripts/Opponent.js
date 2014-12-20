cb3afdef603f61f37c9dd2ecb7113406ac8eafff
Panchamy <kpanchamy@gmail.com>
2014-12-20 10:41:30 +0100

    Added the beta executable and the document
    Beta executable for PC and web
    Fixed minor error in opponent script

--- game/Assets/Scripts/Opponent.js
+++ game/Assets/Scripts/Opponent.js
@@ -22,15 +22,14 @@ function Update () {
 	if (Input.GetKeyDown(KeyCode.Space) && flag == false){
 		var opp = Instantiate(opponent, pos, rot);
 		opp.name = "Opponent";
-		flag = true;
 	}
-	//if (flag == false) {
+	if (flag == false) {
 		var w1 = Instantiate(waypoint1, opponent.transform.position, rot);
 		var w2 = Instantiate(waypoint2, gate.transform.position, rot);
 		w1.name = "Waypoint0";
 		w2.name = "Waypoint1";
-		//flag = true;
-	//}
+		flag = true;
+	}
 	timer = Time.deltaTime;
 	if (flag == true) {
 		Destroy(w1);
