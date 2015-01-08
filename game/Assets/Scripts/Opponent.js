ed1b46857647d4477f8e7d80acb5104bbe5f1365
Panchamy <kpanchamy@gmail.com>
2015-01-08 16:42:01 +0100

    Changes in Opponent AI

--- game/Assets/Scripts/Opponent.js
+++ game/Assets/Scripts/Opponent.js
@@ -1,8 +1,6 @@
 ﻿#pragma strict
 
 var opponent : GameObject;
-var waypoint1 : GameObject;
-var waypoint2 : GameObject;
 var gate : GameObject;
 
 var pos : Vector3;
@@ -23,18 +21,5 @@ function Update () {
 		var opp = Instantiate(opponent, pos, rot);
 		opp.name = "Opponent";
 	}
-	if (flag == false) {
-		var w1 = Instantiate(waypoint1, opponent.transform.position, rot);
-		var w2 = Instantiate(waypoint2, gate.transform.position, rot);
-		w1.name = "Waypoint0";
-		w2.name = "Waypoint1";
-		flag = true;
-	}
-	timer = Time.deltaTime;
-	if (flag == true) {
-		Destroy(w1);
-		Destroy(w2);
-		flag = false;	
-	}
-		
+	timer = Time.deltaTime;		
 }
