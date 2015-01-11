56e808903f1c817adeec2bf8d4b01c11412dcad8
Tiago Pinto <pinttiago@gmail.com>
2015-01-11 02:19:37 +0100

    added posibility to change between first and third person view

--- game/Assets/Scripts/ThirdPersonCameraController.cs
+++ game/Assets/Scripts/ThirdPersonCameraController.cs
@@ -3,15 +3,33 @@ using System.Collections;
 
 public class ThirdPersonCameraController : MonoBehaviour {
 
-	public GameObject player;
-	private Vector3 offset;
+	public Vector3 ThirdPersonView;
+	public Vector3 FirstPersonView;
 
+	private bool isItFPView;
+	/*public GameObject player;
+	private Vector3 offset;*/
+	
 	// Use this for initialization
 	void Start () {
-		offset = transform.position;
+		isItFPView = false;
+		//offset = transform.position;
 	}
-	
+
+	void Update() {
+		float f = Input.GetAxis("Fire2");
+		if(f == 1) {
+			if(isItFPView) {
+				transform.localPosition = ThirdPersonView;
+				isItFPView = false;
+			} else {
+				transform.localPosition = FirstPersonView;
+				isItFPView = true;
+			}
+		}
+	}
+
 	void LateUpdate () {
-		transform.position = player.transform.position + offset;
+		//transform.position = player.transform.position + offset;
 	}
 }
