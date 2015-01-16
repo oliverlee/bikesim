3f17f4b44522ae04ffc6db28c6570a97f361570a
guillecurras <guillecurras@gmail.com>
2015-01-16 16:00:30 +0100

    You die when you hit the trail or the floor, and the game is reset (waiting for Antony's reset function)

---
+++ game/Assets/DieScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieScript : MonoBehaviour {
+
+	bool youHaveDied = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if (youHaveDied) {
+			GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+			GUI.Label (new Rect (0, 0, Screen.width, Screen.height), "WASTED");
+			GUI.skin.label.fontSize = 100;
+			//GUI.skin.label.alignment = TextAnchor.MiddleCenter;
+		}
+	}
+
+	public void setYouHaveDied (bool flag)
+	{
+		youHaveDied = flag;
+	}
+}
