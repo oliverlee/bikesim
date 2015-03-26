96a852ea30698a0a32ca118bdb39eff62a4f8892
Oliver Lee <oliverzlee@gmail.com>
2015-03-26 16:34:00 +0100

    Fix orientation of bicycle controller
    Fix orientation of the bicycle controller contatiner. Due to the Euler angle
    ordering, the lean rotation was applied before the yaw rotation of the
    container object, resulting in significant variations in the pitch of the
    container.

--- bikesim/Assets/Scripts/BicycleController.cs
+++ bikesim/Assets/Scripts/BicycleController.cs
@@ -121,11 +121,11 @@ public class BicycleController : MonoBehaviour {
 	void SetBicycleTransform(VizState q) {
 		// Update x and y positions of the rear wheel contact, yaw and lean of the rear frame
 		// by modifying the transform of root bicycle game object.
-		// As Euler angles use the zxy ordering in Unity, yaw will be applied before lean.
-		// In Unity, a series of rotations R1*R2 will apply R2 after R1.
+		// Explictly apply the yaw and lean rotations.
 		transform.localPosition = new Vector3(q.x, 0.0f, -q.y); // y and z axes are switched
 		transform.localRotation = Quaternion.Euler(90.0f, 0, 0) *
-			Quaternion.Euler(-Mathf.Rad2Deg*q.lean, 0.0f, -Mathf.Rad2Deg*q.yaw);
+			Quaternion.Euler(0.0f, 0.0f, -Mathf.Rad2Deg*q.yaw) *
+			Quaternion.Euler(-Mathf.Rad2Deg*q.lean, 0.0f, 0.0f);
 
 		// All wheel and frame local transforms are with respect to the container game or lean frame
 		//   Update rear wheel angle
