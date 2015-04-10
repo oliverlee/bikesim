87cb12231a4b69db5f6ad1a9db06c852ec81adf6
Oliver Lee <oliverzlee@gmail.com>
2015-04-10 16:47:54 +0200

    Replace tabs with space in Unity3D scripts

--- bikesim/Assets/Scripts/Integrator.cs
+++ bikesim/Assets/Scripts/Integrator.cs
@@ -6,21 +6,21 @@ public delegate Vector<double> IntegratorFunction(double t, Vector<double> y);
 public class Integrator {
     public static Vector<double> RungeKutta4(IntegratorFunction f,
             Vector<double> y0, double t0, double h) {
-		Vector<double> k1 = f(t0, y0);
-		Vector<double> k2 = f(t0 + h/2, y0 + h/2*k1);
-		Vector<double> k3 = f(t0 + h/2, y0 + h/2*k2);
-		Vector<double> k4 = f(t0 + h, y0 + h*k3);
-		return y0 + h/6*(k1 + 2*k2 + 2*k3 + k4);
-	}
+        Vector<double> k1 = f(t0, y0);
+        Vector<double> k2 = f(t0 + h/2, y0 + h/2*k1);
+        Vector<double> k3 = f(t0 + h/2, y0 + h/2*k2);
+        Vector<double> k4 = f(t0 + h, y0 + h*k3);
+        return y0 + h/6*(k1 + 2*k2 + 2*k3 + k4);
+    }
 }
 
 //public class Integrator {
-//	public static T RungeKutta4<S, T>(Func<S, T, T> f, S y0, T t0, T h)
-//	{
-//		T k1 = f(t0, y0);
-//		T k2 = f(t0 + h/2, y0 + h/2*k1);
-//		T k3 = f(t0 + h/2, y0 + h/2*k2);
-//		T k4 = f(t0 + h, y0 + h*k3);
-//		return y0 + h/6*(k1 + 2*k2 + 2*k3 + k4);
-//	}
+//    public static T RungeKutta4<S, T>(Func<S, T, T> f, S y0, T t0, T h)
+//    {
+//        T k1 = f(t0, y0);
+//        T k2 = f(t0 + h/2, y0 + h/2*k1);
+//        T k3 = f(t0 + h/2, y0 + h/2*k2);
+//        T k4 = f(t0 + h, y0 + h*k3);
+//        return y0 + h/6*(k1 + 2*k2 + 2*k3 + k4);
+//    }
 //}
