c61eca9d3bb52263650bbd73f75b86e2354f24cf
Oliver Lee <oliverzlee@gmail.com>
2015-05-08 17:00:56 +0200

    Move serial receive into a separate thread

--- serial2udp.py
+++ serial2udp.py
@@ -13,7 +13,8 @@ from lxml import etree
 DEFAULT_BAUDRATE = 115200
 DEFAULT_ENCODING = 'utf-8'
 DEFAULT_UDPHOST = 'localhost'
-DEFAULT_UDPPORT = 9900
+DEFAULT_UDPRXPORT = 9900
+DEFAULT_UDPRXPORT = 9901
 
 
 class Sample(object):
@@ -22,7 +23,7 @@ class Sample(object):
         self.deltad = deltad
         self.cadence = cadence
         self.brake = brake
-    
+
     def gen_xml(self, enc=DEFAULT_ENCODING):
         root = etree.Element('root')
         etree.SubElement(root, "delta").text = str(self.delta)
@@ -30,7 +31,7 @@ class Sample(object):
         etree.SubElement(root, "cadence").text = str(self.cadence)
         etree.SubElement(root, "brake").text = str(self.brake)
         return etree.tostring(root, encoding=enc)
-        
+
 
 def parse_csv(data):
     vals = data.strip().split(',')
@@ -42,6 +43,12 @@ def parse_csv(data):
     return s
 
 
+def sensor_thread_func(ser, enc, addr, udp):
+    while True:
+        s = parse_csv(ser.readline().decode(enc))
+        udp.sendto(s.gen_xml(), addr)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=
             'Convert serial data in CSV format to XML and send via UDP.')
@@ -50,15 +57,25 @@ if __name__ == "__main__":
                         default=DEFAULT_BAUDRATE, type=int)
     parser.add_argument('-e', '--encoding', help='serial data encoding type',
                         default=DEFAULT_ENCODING)
-    parser.add_argument('-H', '--udp_host', help='udp server host ip',
+    parser.add_argument('-H', '--udp_host', help='udp remote host ip',
                         default=DEFAULT_UDPHOST)
-    parser.add_argument('-P', '--udp_port', help='udp server port',
-                        default=DEFAULT_UDPPORT, type=int)
+    parser.add_argument('-P', '--udp_txport', help='udp tx port',
+                        default=DEFAULT_UDPTXPORT, type=int)
+    parser.add_argument('-p', '--udp_rxport', help='udp rx port',
+                        default=DEFAULT_UDPRXPORT, type=int)
     args = parser.parse_args()
-    
+
     ser = serial.Serial(args.port, args.baudrate)
-    udp_addr = (args.udp_host, args.udp_port)
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_tx_addr = (args.udp_host, args.udp_txport)
+    udp_tx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    sensor_thread = threading.Thread(target=sensor_thread_func,
+            args=(ser, args.encoding, udp_tx_addr, udp_tx))
+    sensor_thread.start()
+
     while True:
-        s = parse_csv(ser.readline().decode(args.encoding))
-        udp.sendto(s.gen_xml(), udp_addr)
+        try:
+            pass
+        except KeyboardInterrupt:
+            break
+    ser.close()
