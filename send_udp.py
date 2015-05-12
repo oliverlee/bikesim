ff5ce45cd81fcb58b39681a57ece2dd4fc94ea7a
Oliver Lee <oliverzlee@gmail.com>
2015-05-12 20:13:04 +0200

    Create both actuator and sensor UDP packets
    In send_udp.py, create either an actuator UDP packet or sensor UDP
    packet depending on number of input arguments supplied.

--- send_udp.py
+++ send_udp.py
@@ -8,20 +8,24 @@ from lxml import etree
 
 
 #usage = "{0} <port> <message>".format(__file__)
-usage = "{0} <port> <delta> <cadence>".format(__file__)
+usage = ("{0} <port> <delta> <deltad> <cadence>".format(__file__) +
+         "\nor\n{0} <port> <torque>".format(__file__))
 
-if len(sys.argv) < 4:
+if len(sys.argv) != 3 and len(sys.argv) != 5:
+    print(len(sys.argv))
     print(usage)
     sys.exit(1)
 
 port = int(sys.argv[1])
 #message = bytearray(sys.argv[2] + "\n", 'utf-8')
-delta = float(sys.argv[2])
-cadence = float(sys.argv[3])
 
 root = etree.Element("root")
-etree.SubElement(root, "delta").text = str(delta)
-etree.SubElement(root, "cadence").text = str(cadence)
+if len(sys.argv) == 3:
+    etree.SubElement(root, "torque").text = str(float(sys.argv[2]))
+else:
+    etree.SubElement(root, "delta").text = str(float(sys.argv[2]))
+    etree.SubElement(root, "deltad").text = str(float(sys.argv[3]))
+    etree.SubElement(root, "cadence").text = str(float(sys.argv[4]))
 message = etree.tostring(root, encoding='utf-8')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
