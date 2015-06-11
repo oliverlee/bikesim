0aaf6d905db57aa636e35f17f5d2601f87bb3df9
Oliver Lee <oliverzlee@gmail.com>
2015-06-11 22:16:41 +0200

    Gracefully exit test_udp_sine.py script

--- comm/test_udp_sine.py
+++ comm/test_udp_sine.py
@@ -9,16 +9,11 @@ import send_udp
 port = 9901
 t0 = time.time()
 
-while True:
-    time.sleep(0.1)
-    t = time.time()
-    y = 2*math.sin(2*(t - t0))
-    send_udp.transmit_udp_xml(port, [str(y)]);
-
-    try:
-        pass
-    except KeyboardInterrupt:
-        break
-
-# TODO: Capture KeyboardInterrupt and send zero torque on exit
-send_udp.transmit_udp_xml(port, ['0'])
+try:
+    while True:
+        time.sleep(0.01)
+        t = time.time()
+        y = 2*math.sin(2*(t - t0))
+        send_udp.transmit_udp_xml(port, [str(y)]);
+except KeyboardInterrupt:
+    send_udp.transmit_udp_xml(port, ['0'])
