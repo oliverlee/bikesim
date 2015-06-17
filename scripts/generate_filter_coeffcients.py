9315377e454a13bd242530b01ad7b2c46edf941e
Oliver Lee <oliverzlee@gmail.com>
2015-06-17 19:47:47 +0200

    Generate filter source files
    As filters are IIR, the filter coefficients must be changed if a
    different order, cutoff freqeuency, or sample frequecy is desired. Add
    jinja2 script to generate these filter source files.

---
+++ scripts/generate_filter_coeffcients.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import sys
+import jinja2
+from scipy.signal import butter, cheby1, cheby2
+
+
+def lowpass_coeffs(filter_type, cutoff, fs, order):
+    nyq = 0.5*fs
+    normal_cutoff = cutoff/nyq
+    a, b = butter(order, normal_cutoff, btype='low', analog=False)
+    return a, b
+
+
+def generate_source(filename_base, ext, template_dict):
+    template = template_setup(ext)
+    scripts_dir = os.path.dirname(os.path.abspath(__file__))
+    source_dir = os.path.join(scripts_dir, '../BsgBikeSim2014')
+    filename = os.path.join(source_dir, filename_base + ext)
+    with open(filename, 'w') as f:
+        f.write(template.render(template_dict))
+
+
+def template_setup(ext):
+    template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'templates')
+    template_loader = jinja2.FileSystemLoader(template_dir)
+    template_env = jinja2.Environment(loader=template_loader)
+    return template_env.get_template('filterlowpass{}.in'.format(ext))
+
+
+if __name__ == "__main__":
+    usage = 'generate arduino source files for filters\n'
+    usage += '{0} <cutoff_freq> <sample_freq>'.format(__file__)
+
+    if len(sys.argv) < 3:
+        print(usage)
+        sys.exit(1)
+
+    cutoff_freq = float(sys.argv[1])
+    sample_freq = float(sys.argv[2])
+    order = 4
+    for filter_type, filter_name in zip((butter, cheby1, cheby2),
+                                        ('butter', 'cheby1', 'cheby2')):
+        a, b = lowpass_coeffs(filter_type, cutoff_freq, sample_freq, order)
+        template_dict = {
+                'filter_name': filter_name.title(),
+                'a': ','.join(map(str, a)),
+                'b': ','.join(map(str, b)),
+                'cutoff_freq': cutoff_freq,
+                'sample_freq': sample_freq,
+                'order': order
+        }
+        generate_source(filter_name.lower() + 'lowpass', '.cpp', template_dict)
+        generate_source(filter_name.lower() + 'lowpass', '.h', template_dict)
