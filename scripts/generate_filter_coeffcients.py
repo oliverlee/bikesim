8795231e50d382bf2ace0e9f96b9a48ed9d95af7
Oliver Lee <oliverzlee@gmail.com>
2015-06-18 13:59:23 +0200

    Add median filter to generation script
    Although median filter is not IIR, generate source with script in case
    the filter order is changed.

--- scripts/generate_filter_coeffcients.py
+++ scripts/generate_filter_coeffcients.py
@@ -14,20 +14,26 @@ def lowpass_coeffs(filter_type, cutoff, fs, order):
 
 
 def generate_source(filename_base, ext, template_dict):
-    template = template_setup(ext)
+    if filename_base is 'median':
+        template = template_setup(ext, filename_base)
+    else:
+        template = template_setup(ext)
     scripts_dir = os.path.dirname(os.path.abspath(__file__))
     source_dir = os.path.join(scripts_dir, '../BsgBikeSim2014')
-    filename = os.path.join(source_dir, filename_base + ext)
+    filename = os.path.join(source_dir, filename_base + 'lowpass' + ext)
     with open(filename, 'w') as f:
         f.write(template.render(template_dict))
 
 
-def template_setup(ext):
+def template_setup(ext, filename_base=None):
     template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 'templates')
     template_loader = jinja2.FileSystemLoader(template_dir)
     template_env = jinja2.Environment(loader=template_loader)
-    return template_env.get_template('filterlowpass{}.in'.format(ext))
+    if filename_base is None:
+        filename_base = 'filter'
+    return template_env.get_template('{}lowpass{}.in'.format(filename_base,
+                                                             ext))
 
 
 if __name__ == "__main__":
@@ -41,16 +47,18 @@ if __name__ == "__main__":
     cutoff_freq = float(sys.argv[1])
     sample_freq = float(sys.argv[2])
     order = 4
+    template_dict = {
+        'cutoff_freq': cutoff_freq,
+        'sample_freq': sample_freq,
+        'order': order
+    }
+    generate_source('median', '.h', template_dict)
+
     for filter_type, filter_name in zip((butter, cheby1, cheby2),
                                         ('butter', 'cheby1', 'cheby2')):
         a, b = lowpass_coeffs(filter_type, cutoff_freq, sample_freq, order)
-        template_dict = {
-                'filter_name': filter_name.title(),
-                'a': ','.join(map(str, a)),
-                'b': ','.join(map(str, b)),
-                'cutoff_freq': cutoff_freq,
-                'sample_freq': sample_freq,
-                'order': order
-        }
-        generate_source(filter_name.lower() + 'lowpass', '.cpp', template_dict)
-        generate_source(filter_name.lower() + 'lowpass', '.h', template_dict)
+        template_dict['filter_name'] = filter_name.title()
+        template_dict['a'] = ','.join(map(str, a))
+        template_dict['b'] = ','.join(map(str, b))
+        generate_source(filter_name.lower(), '.cpp', template_dict)
+        generate_source(filter_name.lower(), '.h', template_dict)
