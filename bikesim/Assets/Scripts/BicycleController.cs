a1ef2977d76d2496bfaedec7cd4d4296c59e3553
Oliver Lee <oliverzlee@gmail.com>
2016-11-16 14:26:16 +0100

    Keep most recent pose on dropped message
    Remove pose message timestamps from display. Keep pose information and
    time differences of most recent drawn frame when messages are dropped.

--- bikesim/Assets/Scripts/BicycleController.cs
+++ bikesim/Assets/Scripts/BicycleController.cs
@@ -128,12 +128,12 @@ public class BicycleController : MonoBehaviour {
 //            Restart(resetCountdownLength);
 //        }
 
-        pose = serial.PopBicyclePose();
         string gitsha1 = serial.gitsha1;
         if (gitsha1 == null) {
             gitsha1 = "null";
         }
 
+        pose = serial.PopBicyclePose();
         if (pose != null) {
             q.SetState(pose, rR, q.wheelAngle, timestamp);
             // There is no operand to add between bytes in C#
@@ -147,17 +147,12 @@ public class BicycleController : MonoBehaviour {
                 "x: {0}\ny: {1}\nroll: {2}\nyaw: {3}\nsteer: {4}",
                 q.x, q.y, q.lean, q.yaw, q.steer);
             sensorInfo.text = System.String.Format(
-                "firmware {4}\ntimestamp 1 {0}\ntimestamp 0 {1}\npose dt: {2} ms\nunity dt: {3} ms",
-                pose.timestamp, timestamp, dt, stopwatch.ElapsedMilliseconds, gitsha1);
+                "firmware {2}\npose dt: {0} ms\nunity dt: {1} ms",
+                dt, stopwatch.ElapsedMilliseconds, gitsha1);
 
             timestamp = pose.timestamp;
             stopwatch.Reset(); // .NET 2.0 doesn't have Stopwatch.Restart()
             stopwatch.Start();
-        } else {
-            stateInfo.text = System.String.Format("pose dropped");
-            sensorInfo.text = System.String.Format(
-                    "gitsha1: {0}\nserial open: {1}",
-                    gitsha1, serial.IsOpen);
         }
 //            sim.wheelRate * rR * 3.6 * -1, // rad/s -> km/hr
 //            sim.feedbackTorque,
